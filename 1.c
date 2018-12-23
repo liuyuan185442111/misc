f3dfe88cba0e2811f9e13a1448db92a1d5d5a03e
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2018-12-23 13:10:25 +0000

    git-svn-id: svn://154.8.199.216/files@6 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- 1.c
+++ 1.c
@@ -1,7 +1,20 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <stddef.h>
+
+static volatile sig_atomic_t sigflag;
+static sigset_t newmask, oldmask, zeromask;
+
+static void sig_usr(int signo)
+{
+	sigflag = 1;
+}
 
 bool TELL_WAIT()
 {
@@ -23,6 +36,7 @@ void WAIT()
     sigprocmask(SIG_SETMASK, &oldmask, NULL);
 }
 
+
 static int update(int *ptr)
 {
 	return ((*ptr)++);
@@ -30,44 +44,39 @@ static int update(int *ptr)
 
 int main()
 {
-        int shmid = shmget(IPC_PRIVATE, sizeof(int), 0600);
-        assert(shmid != -1);
+	int shmid = shmget(IPC_PRIVATE, sizeof(int), 0600);
+	assert(shmid != -1);
 
-        TELL_WAIT2();
+	TELL_WAIT();
 
-        pid_t pid = fork();
-        if(pid < 0) return 3;
-        else if(pid > 0)
-        {
-                void *area = shmat(shmid, 0, 0);
-                assert(area != (void*)-1);
-                printf("parent shm addr: %p\n", area);
-                int tmep;
-                for(int i=0; i<100; i+=2)
-                {
-                        printf("parent %d\n",update((int*)area));
-                        //TELL(pid);
-                        //WAIT();
-                        TELL_CHILD(pid);
-                        WAIT_CHILD();
-                }
-                shmdt(area);
-        }
-        else
-        {
-                void *area = shmat(shmid, 0, 0);
-                assert(area != (void*)-1);
-                printf("child shm addr: %p\n", area);
-                for(int i=1; i<101; i+=2)
-                {
-                        WAIT_PARENT();
-                        //WAIT();
-                        printf("child %d\n",update((int*)area));
-                        TELL_PARENT(getppid());
-                        //TELL(getppid());
-                }
-                shmdt(area);
-                shmctl(shmid, IPC_RMID, 0);
-        }
-        return 0;
+	pid_t pid = fork();
+	if(pid < 0) return 3;
+	else if(pid > 0)
+	{
+		void *area = shmat(shmid, 0, 0);
+		assert(area != (void*)-1);
+		printf("parent shm addr: %p\n", area);
+		for(int i=0; i<100; i+=2)
+		{
+			printf("parent %d\n", update((int*)area));
+			TELL(pid);
+			WAIT();
+		}
+		shmdt(area);
+	}
+	else
+	{
+		void *area = shmat(shmid, 0, 0);
+		assert(area != (void*)-1);
+		printf("child shm addr: %p\n", area);
+		for(int i=1; i<101; i+=2)
+		{
+			WAIT();
+			printf("child %d\n", update((int*)area));
+			TELL(getppid());
+		}
+		shmdt(area);
+		shmctl(shmid, IPC_RMID, 0);
+	}
+	return 0;
 }
