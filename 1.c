bdb82fe8569f495cdfb0c42a9386c2c7bcf19130
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2018-12-23 13:52:24 +0000

    git-svn-id: svn://154.8.199.216/files@7 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- 1.c
+++ 1.c
@@ -1,12 +1,6 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <assert.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <stdbool.h>
 #include <signal.h>
 #include <stddef.h>
+#include "sync.h"
 
 static volatile sig_atomic_t sigflag;
 static sigset_t newmask, oldmask, zeromask;
@@ -16,67 +10,70 @@ static void sig_usr(int signo)
 	sigflag = 1;
 }
 
-bool TELL_WAIT()
+bool TELL_WAIT2()
 {
-    if(signal(SIGUSR1, sig_usr) != 0) return false;
-    if(sigemptyset(&zeromask) != 0) return false;
-    if(sigemptyset(&newmask) != 0) return false;
-    if(sigaddset(&newmask, SIGUSR1) != 0) return false;
-    if(sigprocmask(SIG_BLOCK, &newmask, &oldmask) != 0) return false;
-    return true;
+	struct sigaction act;
+	act.sa_handler = sig_usr;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+    if(sigaction(SIGUSR1, &act, NULL) != 0) return false;
+    if(sigaction(SIGUSR2, &act, NULL) != 0) return false;
+	//if(signal(SIGUSR1, sig_usr) != 0) return false;
+	//if(signal(SIGUSR2, sig_usr) != 0) return false;
+	if(sigemptyset(&zeromask) != 0) return false;
+	if(sigemptyset(&newmask) != 0) return false;
+	if(sigaddset(&newmask, SIGUSR1) != 0) return false;
+	if(sigaddset(&newmask, SIGUSR2) != 0) return false;
+	if(sigprocmask(SIG_BLOCK, &newmask, &oldmask) != 0) return false;
+	return true;
+}
+
+void TELL_PARENT(pid_t pid)
+{
+	kill(pid, SIGUSR1);
+}
+
+void WAIT_PARENT()
+{
+	while(sigflag == 0) sigsuspend(&zeromask);
+	sigflag = 0;
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
+}
+
+void TELL_CHILD(pid_t pid)
+{
+	kill(pid, SIGUSR2);
+}
+
+void WAIT_CHILD()
+{
+	while(sigflag == 0) sigsuspend(&zeromask);
+	sigflag = 0;
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
+}
+
+
+bool TELL_WAIT1()
+{
+	struct sigaction act;
+	act.sa_handler = sig_usr;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+    if(sigaction(SIGUSR1, &act, NULL) != 0) return false;
+	//if(signal(SIGUSR1, sig_usr) != 0) return false;
+	if(sigemptyset(&zeromask) != 0) return false;
+	if(sigemptyset(&newmask) != 0) return false;
+	if(sigaddset(&newmask, SIGUSR1) != 0) return false;
+	if(sigprocmask(SIG_BLOCK, &newmask, &oldmask) != 0) return false;
+	return true;
 }
 void TELL(pid_t pid)
 {
-    kill(pid, SIGUSR1);
+	kill(pid, SIGUSR1);
 }
 void WAIT()
 {
-    while(sigflag == 0) sigsuspend(&zeromask);
-    sigflag = 0;
-    sigprocmask(SIG_SETMASK, &oldmask, NULL);
-}
-
-
-static int update(int *ptr)
-{
-	return ((*ptr)++);
-}
-
-int main()
-{
-	int shmid = shmget(IPC_PRIVATE, sizeof(int), 0600);
-	assert(shmid != -1);
-
-	TELL_WAIT();
-
-	pid_t pid = fork();
-	if(pid < 0) return 3;
-	else if(pid > 0)
-	{
-		void *area = shmat(shmid, 0, 0);
-		assert(area != (void*)-1);
-		printf("parent shm addr: %p\n", area);
-		for(int i=0; i<100; i+=2)
-		{
-			printf("parent %d\n", update((int*)area));
-			TELL(pid);
-			WAIT();
-		}
-		shmdt(area);
-	}
-	else
-	{
-		void *area = shmat(shmid, 0, 0);
-		assert(area != (void*)-1);
-		printf("child shm addr: %p\n", area);
-		for(int i=1; i<101; i+=2)
-		{
-			WAIT();
-			printf("child %d\n", update((int*)area));
-			TELL(getppid());
-		}
-		shmdt(area);
-		shmctl(shmid, IPC_RMID, 0);
-	}
-	return 0;
+	while(sigflag == 0) sigsuspend(&zeromask);
+	sigflag = 0;
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
 }
