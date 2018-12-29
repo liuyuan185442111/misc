f264749a8ad842f0cf876c8889367221b6b5271e
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2018-12-29 07:53:26 +0000

    git-svn-id: svn://154.8.199.216/files@13 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- heap.cpp
+++ heap.cpp
@@ -8,9 +8,9 @@
 #include <cstring>
 using namespace std;
 
-void print_help()
+void print_help(const char *name)
 {
-    cout << "top [gen|set|heap1|heap2|heap3|test]" << endl;
+    cout << name << " [gen|set|heap1|heap2|heap3]" << endl;
 }
 
 //产生count个随机数
@@ -32,9 +32,12 @@ void get_top_set(int n)
     fstream fs("sample.txt", fstream::in);
     for(int t; fs >> t;)
     {
-        top.insert(t);
-        if(top.size() > n)
-            top.erase(top.begin());
+		if(t > *top.begin())
+		{
+			top.insert(t);
+			if(top.size() > n)
+				top.erase(top.begin());
+		}
     }
     fs.close();
     ostream_iterator<int> out_it(std::cout, ",");
@@ -42,26 +45,27 @@ void get_top_set(int n)
     cout << endl;
 }
 
-/** 使用STL提供的堆相关的函数获得top n, 这里依赖库的实现方式, 实际使用前需要进行测试
-或者自行实现堆算法, 参考 https://blog.csdn.net/liuyuan185442111/article/details/45920979
-pop_heap的gcc实现大概是先将first和last-1位置的值互换，这样first位置的值就是不对的了，
+/** 使用STL提供的堆相关的函数获得top n, 这里依赖库的实现方式, 实际使用前需要进行确认,
+或自行实现堆算法, 参考 https://blog.csdn.net/liuyuan185442111/article/details/45920979
+pop_heap的gcc实现大概是先将first和last-1位置的值互换，这样first位置的值就不一定是对的了，
 然后调用__adjust_heap将[first, last-1)调整为堆。
-所以gcc的pop_heap实际上只需要[first,last-1)是一个合法的堆。*/
+所以gcc的pop_heap实际上只要求[first,last-1)是一个合法的堆。*/
 void get_top_heap1(int n)
 {
     fstream fs("sample.txt", fstream::in);
-    int top[n+2];
-    for(int i=0; i<n+1; ++i)
+    int top[n+1];
+    for(int i=0; i<n; ++i)
     {
         if(!(fs >> top[i])) return;
     }
-    make_heap(top, top+n+1, greater<int>());
-    while(fs >> top[n+1])
+    make_heap(top, top+n, greater<int>());
+	int *top_end = top+n+1;
+    while(fs >> top[n])
     {
-        pop_heap(top, top+n+2, greater<int>());
+		if(top[n] > top[0])
+			pop_heap(top, top_end, greater<int>());
     }
     fs.close();
-    pop_heap(top, top+n+1, greater<int>());
     sort_heap(top, top+n, greater<int>());
     ostream_iterator<int> out_it(std::cout, ",");
     copy(top, top+n, out_it);
@@ -73,20 +77,20 @@ void get_top_heap1(int n)
 void get_top_heap2(int n)
 {
     fstream fs("sample.txt", fstream::in);
-    int top[n+1];
-    for(int i=0; i<n+1; ++i)
+    int top[n];
+    for(int i=0; i<n; ++i)
     {
         if(!(fs >> top[i])) return;
     }
-    make_heap(top, top+n+1, greater<int>());
+    make_heap(top, top+n, greater<int>());
     for(int t; fs >> t;)
     {
         //取出堆的根节点, 将t作为新的根节点, 然后进行调整
         //上面直接用pop_heap的话实际上是将top[n+1]作为t的存放处
-        __adjust_heap(top, 0, n+1, t, greater<int>());
+		if(t > top[0])
+			__adjust_heap(top, 0, n, t, greater<int>());
     }
     fs.close();
-    pop_heap(top, top+n+1, greater<int>());
     sort_heap(top, top+n, greater<int>());
     ostream_iterator<int> out_it(std::cout, ",");
     copy(top, top+n, out_it);
@@ -98,36 +102,36 @@ void get_top_heap2(int n)
 template <typename Key, int maxsize, typename Cmp = std::greater<Key> >
 class TopTable
 {
-    int top[maxsize+1];
+    int top[maxsize];
     int size;
+	Cmp comparator;
 public:
     TopTable():size(0){}
-    void Push(const Key &key)
+    bool Push(const Key &key)
     {
-        if(size < maxsize+1)
+        if(size < maxsize)
         {
             top[size++] = key;
-            if(size == maxsize+1)
-                make_heap(top, top+size, Cmp());
-            return;
+            if(size == maxsize)
+                make_heap(top, top+size, comparator);
+            return true;
         }
-        __adjust_heap(top, 0, maxsize+1, key, Cmp());
+		if(comparator(key, top[0]))
+		{
+			__adjust_heap(top, 0, maxsize, key, comparator);
+			return true;
+		}
+		return false;
     }
-    void Build(std::vector<Key> &table)
+    int Build(std::vector<Key> &table)
     {
-        if(size == maxsize+1)
-        {
-            pop_heap(top, top+maxsize+1, Cmp());
-            sort_heap(top, top+maxsize, Cmp());
-            table.reserve(maxsize);
-            table.assign(top, top+maxsize);
-        }
+        if(size < maxsize)
+            sort(top, top+size, comparator);
         else
-        {
-            sort(top, top+size);
-            table.reserve(size);
-            table.assign(top, top+size);
-        }
+            sort_heap(top, top+size, comparator);
+		table.reserve(size);
+		table.assign(top, top+size);
+		return size;
     }
 };
 
@@ -136,9 +140,7 @@ void get_top500()
     TopTable<int, 500> table;
     fstream fs("sample.txt", fstream::in);
     for(int t; fs >> t; )
-    {
         table.Push(t);
-    }
     fs.close();
     std::vector<int> top;
     table.Build(top);
@@ -161,15 +163,10 @@ int main(int argc, char **argv)
             get_top_heap2(500);
         else if(strcmp(argv[1], "heap3") == 0)
             get_top500();
-        else if(strcmp(argv[1], "test") == 0)
-        {
-            clock_t t = clock();
-            cout << t << endl;
-        }
         else
-            print_help();
+            print_help(argv[0]);
         return 0;
     }
-    print_help();
+    print_help(argv[0]);
     return 0;
 }
