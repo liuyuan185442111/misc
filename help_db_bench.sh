faeba7fb7a996310e15f6fdc1b34f4a93f05ae94
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2019-06-11 10:08:19 +0000

    git-svn-id: svn://154.8.199.216/files@109 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- help_db_bench.sh
+++ help_db_bench.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+#file args like:
+#--threads=2 --num=10000 --value_size=1024 --write_buffer_size=4048576 --benchmarks=fillrandom
 if [ $# -eq 0 ];then
-	echo $0 "need parameters"
-	exit 1
+	xargs -a args $0
+	exit $?
 fi
-name=${1// /_}
+name=$*
+name=${name//_/}
+name=${name// /_}
 name=${name//-/}
 name=${name//=/}
 if [ -f $name ];then
 	echo $name "exists"
 	exit 2
 fi
-env time ./db_bench --histogram=1 $1 >$name 2>${name}.err
+env time ./db_bench $* --histogram=1 >$name 2>${name}.err
 tail -2 ${name}.err >> $name
