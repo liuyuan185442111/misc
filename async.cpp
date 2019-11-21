046aba547249995c52400b4856227947028f3aec
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2019-11-21 11:09:51 +0000

    git-svn-id: svn://154.8.199.216/files@138 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- async.cpp
+++ async.cpp
@@ -4,7 +4,6 @@
 
 bool is_prime(int x)
 {
-    std::cout << "task thread id is " << std::this_thread::get_id() << std::endl;
     for (int i = 2; i < x; ++i)
         if (x % i == 0)
             return false;
@@ -13,25 +12,17 @@ bool is_prime(int x)
 
 int main()
 {
-    std::cout << "main thread id is " << std::this_thread::get_id() << std::endl;
-    // call function asynchronously:
-    //std::future<bool> fut = std::async(std::launch::async, is_prime, 1610612741);
-    //std::future<bool> fut = std::async(std::launch::deferred, is_prime, 1610612741);
-    std::future<bool> fut = std::async(is_prime, 1610612741);
-    /**async不带参数等同于带std::launch::async|std::launch::deferred
-    std::launch::deferred实际一个线程
-    */
-
-    // do something while waiting for function to set future:
+    std::future<bool> fut = std::async(std::launch::deferred, is_prime, 1610612741);
     std::cout << "checking, please wait\n";
-    std::chrono::milliseconds span(100);
-    /** 如指定std::launch::deferred, fut.wait_for(span)应返回std::future_status::deferred,
+    /** fut.wait_for()应返回std::future_status::deferred,
     但gcc version 4.8.2却返回std::future_status::timeout */
-    while(fut.wait_for(span) == std::future_status::timeout)
+    while(fut.wait_for(std::chrono::milliseconds(100)) == std::future_status::timeout)
+    {
         std::cout << '.';
-
-    std::cout << std::endl;
-    bool x = fut.get(); // retrieve return value
+        fflush(stdout);
+    }
+    std::cout << "return of wait_for\n";
+    bool x = fut.get();
     std::cout << (x ? "is" : "is not") << " prime.\n";
 
     return 0;
