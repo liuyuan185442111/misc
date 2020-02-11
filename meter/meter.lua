0b8600a61e0a2031aaf8b33dfd720b5d45759304
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-02-11 10:26:32 +0000

    git-svn-id: svn://154.8.199.216/files@156 91d82e07-cc47-4fd4-84ae-454f48a309b4

---
+++ meter/meter.lua
@@ -0,0 +1,18 @@
+#! /bin/env lua
+battle = {}
+curr = {}
+curr.source = 1
+curr.target = 2
+curr.sourcecamp = 1
+curr.targetcamp = 2
+curr.value = 10
+curr.skill = 2
+curr.heal = 34
+curr.damage = 456
+curr.overheal = 34
+curr.overdamage = 456
+curr.flag = 1
+
+function pushback()
+table.insert()
+end
