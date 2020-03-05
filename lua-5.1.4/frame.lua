57c569648e9ee618ff549d00778af79f0f33746d
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-03-05 03:04:57 +0000

    git-svn-id: svn://154.8.199.216/files@203 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- lua-5.1.4/frame.lua
+++ lua-5.1.4/frame.lua
@@ -1,123 +1,6 @@
-swin = {
-	battle,mode,id,subid,frame
-}
+local AllFrames = {}
 
-click调用comeon，返回true再调用update
-goback重置frame即可，不必将四个参数都还原
-item身上只存swin id和pos即可
-
-sum = {}
-table.insert(sum, {
-	index = '',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-		if pos == 0 then
-			win.battle = currbattle
-		elseif pos == 1 then
-			win.battle = sumbattle
-		else
-			pos = pos - 2
-			if pos>=0 and pos<=#allbattle then
-				win.battle = allbattle[pos]
-			else
-				return false
-			end
-		end
-		return true
-	end,
-})
-table.insert(sum, {
-	index = '1',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-		if pos>=0 and pos<=10 then
-			win.mode = pos
-			return true
-		end
-		return false
-	end,
-})
-table.insert(sum, {
-	index = '1a',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1a1',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1a11',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1a12',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1b',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1b1',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1b11',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-table.insert(sum, {
-	index = '1b12',
-	next = {},
-	update = function()
-	end,
-	comeon = function(win, pos)
-	end,
-})
-
-table.sort(sum, function(a,b)
-	if #a.index<#b.index then
-		return true
-	else
-		if #a.index==#b.index then
-			return a.index<b.index
-		end
-	end
-end)
-skada.reverse_array(sum)
-
-function find(t, s)
+local function find_and_remove(t, s)
 	for i=#t,1,-1 do
 		if t[i].index == s then
 			local r = t[i]
@@ -126,32 +9,202 @@ function find(t, s)
 		end
 	end
 end
-root = find(sum, '')
-
-function getchar(i)
+local function getchar(i)
 	return string.char(string.byte('abcdefghijklmnopqrstuvwxyz', i))
 end
-
-function recurse(root)
+local function addchildren(root, sum)
 	if #sum == 0 then return end
-	root.left = find(sum, root.index .. '1')
-	if root.left then
-		recurse(root.left)
+	local temp = find_and_remove(sum, root.index .. '1')
+	if temp then
+		root.left = temp
+		addchildren(temp, sum)
 	end
-	root.right = find(sum, root.index .. '2')
-	if root.right then
-		recurse(root.right)
+	temp = find_and_remove(sum, root.index .. '2')
+	if temp then
+		root.right = temp
+		addchildren(temp, sum)
 	end
 	for i=1,16 do
-		local temp = find(sum, root.index .. getchar(i))
+		temp = find_and_remove(sum, root.index .. getchar(i))
 		if temp then
 			root.next[i] = temp
-			recurse(root.next[i])
+			addchildren(temp, sum)
 		end
 	end
 end
 
-recurse(root)
+local function BuildFrameTree()
+	local sum = {}
+	table.insert(sum, {
+		index = '',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+			if pos == 0 then
+				win.battle = currbattle
+			elseif pos == 1 then
+				win.battle = sumbattle
+			else
+				pos = pos - 2
+				if pos>=0 and pos<=#allbattle then
+					win.battle = allbattle[pos]
+				else
+					return false
+				end
+			end
+			return true
+		end,
+	})
+	table.insert(sum, {
+		index = '1',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+			if pos>=0 and pos<=10 then
+				win.mode = pos
+				return true
+			end
+			return false
+		end,
+	})
+	table.insert(sum, {
+		index = '1a',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1a1',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1a11',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1a12',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1b',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1b1',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1b11',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
+	table.insert(sum, {
+		index = '1b12',
+		next = {},
+		update = function()
+		end,
+		comeon = function(win, pos)
+		end,
+	})
 
-print(skada.dump(sum))
-print(skada.dump(root))
+	table.sort(sum, function(a,b)
+		if #a.index<#b.index then
+			return true
+		end
+		if #a.index==#b.index then
+			return a.index<b.index
+		end
+		return false
+	end)
+
+	AllFrames = skada.clone_array(sum)
+	skada.reverse_array(sum)
+	local root = find_and_remove(sum, '')
+	addchildren(root, sum)
+	return root
+end
+
+local RootFrame = BuildFrameTree()
+
+local function NewWindow(index)
+	local widget = 'CreateWidget()'
+	return {
+		frame = RootFrame,
+		widget = widget,
+		--battle,mode,id,subid are nil
+	}
+end
+local SWin = {}
+table.insert(SWin, NewWindow())
+table.insert(SWin, NewWindow())
+table.insert(SWin, NewWindow())
+
+local FrameStack = {}
+
+local function onleftclick(winid, pos)
+	local win = SWin[winid]
+	local currframe = win.frame
+	local nextframe = currframe.left or currframe.next[pos]
+	if not nextframe then
+		return false
+	end
+	table.insert(FrameStack, currframe)
+	win.frame = nextframe
+	win.frame.comeon(win, pos)
+	win.frame.update(win.widget, win)
+	return true
+end
+local function onrightclick(winid, pos)
+	local win = SWin[winid]
+	local currframe = win.frame
+	local nextframe = currframe.right
+	if not nextframe then
+		return false
+	end
+	table.insert(FrameStack, currframe)
+	win.frame = nextframe
+	win.frame.comeon(win, pos)
+	win.frame.update(win.widget, win)
+	return true
+end
+local function ongoback(winid, pos)
+	if #FrameStack == 0 then
+		return false
+	end
+	local win = SWin[winid]
+	win.frame = FrameStack[#FrameStack]
+	table.remove(FrameStack)
+	win.frame.update(win.widget, win)
+	return true
+end
+local function ontick(winid)
+	local win = SWin[winid]
+	win.frame.update(win.widget, win)
+end
