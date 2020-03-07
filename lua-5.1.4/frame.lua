bcb2b8f4f2c2ae2d2729a8ae70c18b4e00a3067d
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-03-07 14:33:51 +0000

    git-svn-id: svn://154.8.199.216/files@209 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- lua-5.1.4/frame.lua
+++ lua-5.1.4/frame.lua
@@ -1,210 +1,43 @@
-local AllFrames = {}
-
-local function find_and_remove(t, s)
-	for i=#t,1,-1 do
-		if t[i].index == s then
-			local r = t[i]
-			table.remove(t, i)
-			return r
-		end
+#!/bin/env lua
+require('common')
+require('port')
+role = {}
+buff = {}
+function addbuff(xid, buffid, favorable)
+	local temp = role[xid]
+	if temp == nil then
+		temp = {}
+		role[xid] = temp
 	end
-end
-local function getchar(i)
-	return string.char(string.byte('abcdefghijklmnopqrstuvwxyz', i))
-end
-local function addchildren(root, sum)
-	if #sum == 0 then return end
-	local temp = find_and_remove(sum, root.index .. '1')
-	if temp then
-		root.left = temp
-		addchildren(temp, sum)
+	local temp2 = temp[buffid]
+	if temp2 == nil then
+		temp2 = {id=buffid,time=0}
+		temp[buffid] = temp2
 	end
-	temp = find_and_remove(sum, root.index .. '2')
-	if temp then
-		root.right = temp
-		addchildren(temp, sum)
+	temp2.begin = skada.nowtime()
+
+	temp = buff[buffid]
+	if temp == nil then
+		temp = {}
+		buff[buffid] = temp
 	end
-	for i=1,16 do
-		temp = find_and_remove(sum, root.index .. getchar(i))
-		if temp then
-			root.next[i] = temp
-			addchildren(temp, sum)
-		end
+	temp[xid] = true 
+end
+
+function delbuff(xid, buffid)
+	local temp = role[xid]
+	if temp == nil then
+		return
 	end
-end
-
-local function BuildFrameTree()
-	local sum = {}
-	table.insert(sum, {
-		index = '',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-			if pos == 0 then
-				win.battle = currbattle
-			elseif pos == 1 then
-				win.battle = sumbattle
-			else
-				pos = pos - 2
-				if pos>=0 and pos<=#allbattle then
-					win.battle = allbattle[pos]
-				else
-					return false
-				end
-			end
-			return true
-		end,
-	})
-	table.insert(sum, {
-		index = '1',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-			if pos>=0 and pos<=10 then
-				win.mode = pos
-				return true
-			end
-			return false
-		end,
-	})
-	table.insert(sum, {
-		index = '1a',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1a1',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1a11',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1a12',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1b',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1b1',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1b11',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-	table.insert(sum, {
-		index = '1b12',
-		next = {},
-		update = function()
-		end,
-		comeon = function(win, pos)
-		end,
-	})
-
-	table.sort(sum, function(a,b)
-		if #a.index<#b.index then
-			return true
-		end
-		if #a.index==#b.index then
-			return a.index<b.index
-		end
-		return false
-	end)
-
-	AllFrames = skada.clone_array(sum)
-	skada.reverse_array(sum)
-	local root = find_and_remove(sum, '')
-	addchildren(root, sum)
-	return root
-end
-
-local RootFrame = BuildFrameTree()
-
-local function NewWindow(index)
-	local widget = 'CreateWidget()'
-	return {
-		frame = RootFrame,
-		widget = widget,
-		--battle,mode,id,subid are nil
-	}
-end
-local SWin = {}
-table.insert(SWin, NewWindow())
-table.insert(SWin, NewWindow())
-table.insert(SWin, NewWindow())
-
-local FrameStack = {}
-
-local function onleftclick(winid, pos)
-	local win = SWin[winid]
-	local currframe = win.frame
-	local nextframe = currframe.left or currframe.next[pos]
-	if not nextframe then
-		return false
+	local temp2 = temp[buffid]
+	if temp2 == nil then
+		return
 	end
-	table.insert(FrameStack, currframe)
-	win.frame = nextframe
-	win.frame.comeon(win, pos)
-	win.frame.update(win.widget, win)
-	return true
+	temp2.time = temp2.time + skada.nowtime() - temp2.begin
+	temp2.begin = 0
 end
-local function onrightclick(winid, pos)
-	local win = SWin[winid]
-	local currframe = win.frame
-	local nextframe = currframe.right
-	if not nextframe then
-		return false
-	end
-	table.insert(FrameStack, currframe)
-	win.frame = nextframe
-	win.frame.comeon(win, pos)
-	win.frame.update(win.widget, win)
-	return true
-end
-local function ongoback(winid, pos)
-	if #FrameStack == 0 then
-		return false
-	end
-	local win = SWin[winid]
-	win.frame = FrameStack[#FrameStack]
-	table.remove(FrameStack)
-	win.frame.update(win.widget, win)
-	return true
-end
-local function ontick(winid)
-	local win = SWin[winid]
-	win.frame.update(win.widget, win)
-end
+
+addbuff('a', 1)
+delbuff('a', 1)
+print(skada.dump(role, 'role = '))
+print(skada.dump(buff, 'buff = '))
