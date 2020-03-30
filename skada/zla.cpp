988aaafb4fda1eae61eb247c37a2621bf143bd76
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-03-30 10:55:49 +0000

    git-svn-id: svn://154.8.199.216/files@258 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- skada/zla.cpp
+++ skada/zla.cpp
@@ -2,60 +2,111 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <functional>
 using namespace std;
 
 using int64 = long;
 
 namespace std
 {
-	template <typename T1, typename T2>
-	struct hash<pair<T1, T2>>
+template <>
+struct hash<pair<int64, int>>
+{
+	size_t operator()(pair<int64, int> val) const
 	{
-		size_t operator()(pair<T1,T2> val) const
-		{
-			return hash<T1>(val.first) + hash<T2>(val.second);
-		}
-	};
+		return hash<int64>()(val.first) + hash<int>()(val.second);
+	}
+};
 }
 
-struct elem
+struct BuffCover
 {
-	bool active;
-	int64 lastaddtime;//上次添加时间
-	int covertime;//覆盖时间
-	int coverage;//覆盖率
-};
-std::unordered_map<std::pair<int64, int>, elem> pools;
+	struct elem
+	{
+		bool active;
+		int64 lastaddtime;//上次添加时间
+		int period;
+		int covertime;//覆盖时间
+		int coverage;//覆盖率
+	};
+	std::unordered_map<std::pair<int64, int>, elem> pools;
 
-struct buff_set_content
-{
-	bool active;
-	std::unordered_set<int64> roles;
-	std::vector<std::pair<int, int64>> sorted_roles;//按覆盖率排序的roles
-};
-std::unordered_map<int, buff_set_content> buff_set;
-struct buff_sort_content
-{
-	int buffid;
-	int count;
-};
-std::vector<buff_sort_content> buff_sort;//按数量排序的buffs，由buff_set得来
-std::vector<buff_sort_content> debuff_sort;//按数量排序的debuffs，由buff_set得来
+	struct buff_set_content
+	{
+		bool active;
+		std::unordered_set<int64> roles;
+		std::vector<std::pair<int, int64>> sorted_roles;//按覆盖率排序的roles
+	};
+	std::unordered_map<int, buff_set_content> buff_set;
+	struct buff_sort_content
+	{
+		int buffid;
+		int count;
+	};
+	std::vector<buff_sort_content> buff_sort;//按数量排序的buffs，由buff_set得来
+	std::vector<buff_sort_content> debuff_sort;//按数量排序的debuffs，由buff_set得来
 
-struct role_set_content
-{
-	std::unordered_set<int> buffs;
-	std::vector<std::pair<int, int>> sorted_buffs;//按覆盖率排序的增益
-	std::vector<std::pair<int, int>> sorted_debuffs;//按覆盖率排序的减益
+	struct role_set_content
+	{
+		std::unordered_set<int> buffs;
+		std::vector<std::pair<int, int>> sorted_buffs;//按覆盖率排序的增益
+		std::vector<std::pair<int, int>> sorted_debuffs;//按覆盖率排序的减益
+	};
+	std::unordered_map<int64, role_set_content> role_set;
+	struct role_sort_content
+	{
+		int64 roleid;
+		int coverage;//平均覆盖率
+		int count;//数量
+	};
+	std::vector<role_sort_content> role_sort;//按平均覆盖率排序的roles，由role_set得来
+
+	bool OK = false;
+
+	void addbuff(int64 roleid, int buffid, int period)
+	{
+	}
+	void delbuff(int64 roleid, int buffid)
+	{
+	}
+	bool calbuff()
+	{
+		OK = true;
+	}
+	//一个名字和职业的缓存
+	//数据的导入导出
 };
-std::unordered_map<int64, role_set_content> role_set;
-struct role_sort_content
+
+struct Battle
 {
-	int64 roleid;
-	int coverage;//平均覆盖率
-	int count;//数量
+	BuffCover _buff;
 };
-std::vector<role_sort_content> role_sort;//按平均覆盖率排序的roles，由role_set得来
+
+struct UZCSkadaManager
+{
+	//0放当前 1放总计
+	std::unordered_map<int64, Battle> allbattle;
+
+	void finish_curr_battle()
+	{
+	}
+	void begin_curr_battle()
+	{
+	}
+	void remove_battle()
+	{
+	}
+
+	void addbuff(int64 roleid, int buffid, int period)
+	{
+	}
+	void delbuff(int64 roleid, int buffid)
+	{
+	}
+	//todo 查询接口
+
+	//数据的导入导出
+};
 
 int main()
 {
