0ac1c69adc726b86e51b0715578cb5799462a988
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-03-31 03:43:32 +0000

    git-svn-id: svn://154.8.199.216/files@263 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- skada/skada.cpp
+++ skada/skada.cpp
@@ -1,13 +1,16 @@
 #include "skada.h"
 
-struct SkadaManager
+class SkadaManager
 {
+private:
 	int64 currbattle = 0;
 	std::unordered_map<int64, SkadaCpp::Battle> allbattle; //0是总计
 
+public:
 	void begin_battle(int64 battle)
 	{
 		currbattle = battle;
+		allbattle[currbattle].begin(battle);
 	}
 	void finish_battle()
 	{
@@ -30,8 +33,6 @@ struct SkadaManager
 		if(currbattle == 0) return;
 		allbattle[currbattle]._buff.delbuff(roleid, buffid);
 	}
-	//查询接口
-	//数据的导入导出
 };
 
 int main()
@@ -48,11 +49,11 @@ namespace SkadaCpp {
 
 void BuffCover::addbuff(int64 roleid, int buffid, int64 addtime, int period)
 {
-	auto &elem = pools[std::make_pair(roleid, buffid)];
+	auto &elem = pool[std::make_pair(roleid, buffid)];
 	if(elem.addtime > 0) //未del便add
 	{
 		int64 nowtime = time(nullptr);
-		elem.covertime = elem.lasttime + std::min(nowtime - elem.addtime, (int64)elem.period);
+		elem.lasttime = elem.lasttime + std::min(nowtime - elem.addtime, (int64)elem.period);
 	}
 	elem.addtime = addtime;
 	elem.period = period;
@@ -62,9 +63,10 @@ void BuffCover::addbuff(int64 roleid, int buffid, int64 addtime, int period)
 
 void BuffCover::delbuff(int64 roleid, int buffid)
 {
-	auto iter = pools.find(std::make_pair(roleid, buffid));
-	if(iter == pools.end())
+	auto iter = pool.find(std::make_pair(roleid, buffid));
+	if(iter == pool.end())
 	{
+		//TODO 此时也可能是进入战斗后没收到addbuff，考虑使用buff的period而不是直接用starttime
 		addbuff(roleid, buffid, starttime, 0);
 		delbuff(roleid, buffid);
 	}
@@ -74,8 +76,12 @@ void BuffCover::delbuff(int64 roleid, int buffid)
 		if(elem.addtime > 0)
 		{
 			int64 nowtime = time(nullptr);
-			elem.covertime = elem.lasttime + nowtime - elem.addtime;
+			elem.lasttime = elem.lasttime + nowtime - elem.addtime;
 			elem.addtime = 0;
+		}
+		else
+		{
+			//TODO 可能未收到addbuff，考虑使用该buff的period计算时长
 		}
 	}
 }
@@ -84,7 +90,8 @@ bool BuffCover::calbuff(bool finish)
 {
 	if(OK) return false;
 	if(finish) OK = true;
-	for(auto &e : pools)
+
+	for(auto &e : pool)
 	{
 		auto &elem = e.second;
 		if(elem.active == 0)
@@ -96,96 +103,116 @@ bool BuffCover::calbuff(bool finish)
 			int64 nowtime = time(nullptr);
 			elem.covertime = elem.lasttime + std::min(nowtime - elem.addtime, (int64)elem.period);
 		}
+		else
+		{
+			elem.covertime = elem.lasttime;
+		}
+		elem.coverage = elem.covertime / 1;
 		if(finish)
 		{
 			elem.addtime = 0;
 			elem.lasttime = 0;
+			elem.period = 0;
 		}
-		elem.coverage = elem.covertime / 1;
 	}
 
+	buff_sort.clear();
+	debuff_sort.clear();
 	for(auto &e : buff_set)
 	{
+		if(e.second.active == 0)
+		{
+			e.second.active = 1;
+		}
+		int buffid = e.first;
 		auto &roles = e.second.roles;
-		auto &sorted_roles = e.second.sorted_roles;
 		if(e.second.active == 1)
 		{
-			buff_sort.emplace_back(e.first, (int)roles.size());
+			buff_sort.emplace_back(buffid, (int)roles.size());
 		}
-		else
+		else if(e.second.active == 2)
 		{
-			debuff_sort.emplace_back(e.first, (int)roles.size());
+			debuff_sort.emplace_back(buffid, (int)roles.size());
 		}
+		auto &sorted_roles = e.second.sorted_roles;
 		sorted_roles.clear();
 		sorted_roles.reserve(roles.size());
-		auto key = std::make_pair((int64)0, e.first);
-		for(auto f : roles)
+		auto key = std::make_pair((int64)0, buffid);
+		for(auto roleid : roles)
 		{
-			key.first = f;
-			sorted_roles.push_back(std::make_pair(pools[key].coverage, f));
+			key.first = roleid;
+			sorted_roles.emplace_back(pool[key].coverage, roleid);
 		}
-		using T = typename std::remove_reference<decltype(sorted_roles)>::type::const_reference;
+		using T = typename decltype(e.second.sorted_roles)::const_reference;
 		std::sort(sorted_roles.begin(), sorted_roles.end(), [](T a, T b){return a.first>b.first;});
 	}
-	using T = typename std::remove_reference<decltype(buff_sort)>::type::const_reference;
-	std::sort(buff_sort.begin(), buff_sort.end(), [](T a, T b){return a.count>b.count;});
-	std::sort(debuff_sort.begin(), debuff_sort.end(), [](T a, T b){return a.count>b.count;});
+	{
+		using T = typename decltype(buff_sort)::const_reference;
+		std::sort(buff_sort.begin(), buff_sort.end(), [](T a, T b){return a.count>b.count;});
+		std::sort(debuff_sort.begin(), debuff_sort.end(), [](T a, T b){return a.count>b.count;});
+	}
 
+	role_sort1.clear();
+	role_sort2.clear();
 	for(auto &e : role_set)
 	{
-		auto &content = e.second;
-		auto key = std::make_pair(e.first, 0); 
+		int64 roleid = e.first;
+		auto &sorted_buffs = e.second.sorted_buffs;
+		auto &sorted_debuffs = e.second.sorted_debuffs;
+		auto key = std::make_pair(roleid, 0); 
 		int count1 = 0, count2 = 0;
 		int coverage1 = 0, coverage2 = 0;
-		for(auto &f : content.buffs)
+		for(auto &buffid : e.second.buffs)
 		{
-			key.second = f;
-			const auto &elem = pools[key];
+			key.second = buffid;
+			const auto &elem = pool[key];
 			if(elem.active == 1)
 			{
 				++count1;
 				coverage1 += elem.coverage;
-				content.sorted_buffs.push_back(std::make_pair(elem.coverage, f));
+				sorted_buffs.emplace_back(elem.coverage, buffid);
 			}
-			else
+			else if(elem.active == 2)
 			{
 				++count2;
 				coverage2 += elem.coverage;
-				content.sorted_debuffs.push_back(std::make_pair(elem.coverage, f));
+				sorted_debuffs.emplace_back(elem.coverage, buffid);
 			}
 		}
-		using T = typename decltype(content.sorted_buffs)::const_reference;
-		std::sort(content.sorted_buffs.begin(), content.sorted_buffs.end(), [](T a, T b){return a.first>b.first;});
-		std::sort(content.sorted_debuffs.begin(), content.sorted_debuffs.end(), [](T a, T b){return a.first>b.first;});
-		role_sort1.clear();
-		role_sort1.emplace_back(e.first, coverage1, count1);
-		role_sort2.clear();
-		role_sort2.emplace_back(e.first, coverage2, count2);
+		using T = typename decltype(e.second.sorted_buffs)::const_reference;
+		std::sort(sorted_buffs.begin(), sorted_buffs.end(), [](T a, T b){return a.first>b.first;});
+		std::sort(sorted_debuffs.begin(), sorted_debuffs.end(), [](T a, T b){return a.first>b.first;});
+		role_sort1.emplace_back(roleid, coverage1, count1);
+		role_sort2.emplace_back(roleid, coverage2, count2);
 	}
-	using U = typename decltype(role_sort1)::const_reference;
-	std::sort(role_sort1.begin(), role_sort1.end(), [](U a, U b){return a.coverage>b.coverage;});
-	std::sort(role_sort2.begin(), role_sort2.end(), [](U a, U b){return a.coverage>b.coverage;});
+	{
+		using T = typename decltype(role_sort1)::const_reference;
+		std::sort(role_sort1.begin(), role_sort1.end(), [](T a, T b){return a.avgcoverage>b.avgcoverage;});
+		std::sort(role_sort2.begin(), role_sort2.end(), [](T a, T b){return a.avgcoverage>b.avgcoverage;});
+	}
 	return true;
 }
 
 BuffCover& BuffCover::operator+=(const BuffCover &cover)
 {
-	for(const auto &e : cover.pools)
+	for(const auto &e : cover.pool)
 	{
-		pools[e.first].covertime += e.second.covertime;
+		pool[e.first].covertime += e.second.covertime;
 	}
 	for(const auto &e : cover.buff_set)
 	{
-		auto &t = buff_set[e.first];
-		for(const auto &f : e.second.roles)
-			t.roles.insert(f);
+		auto &roles = buff_set[e.first].roles;
+		for(auto role : e.second.roles)
+			roles.insert(role);
 	}
 	for(const auto &e : cover.role_set)
 	{
-		auto &t = role_set[e.first];
-		for(const auto &f : e.second.buffs)
-			t.buffs.insert(f);
+		auto &buffs = role_set[e.first].buffs;
+		for(auto buff : e.second.buffs)
+			buffs.insert(buff);
 	}
+	starttime = 0;
+	OK = false;
 	return *this;
 }
 
--- skada/skada.h
+++ skada/skada.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
@@ -6,86 +8,81 @@
 
 using int64 = long;
 
-//不要在std里写东西了
-namespace std
-{
-template <>
-struct hash<pair<int64, int>>
-{
-	size_t operator()(pair<int64, int> val) const
-	{
-		return hash<int64>()(val.first) + hash<int>()(val.second);
-	}
-};
-}
-
 namespace SkadaCpp {
 
 class BuffCover
 {
 private:
-	struct elem
+	struct Elem
 	{
-		int active = 0;//1:有益 2:有害
+		int active = 0;//1:有利 2:不利
 		int64 addtime = 0;//上次添加时间
 		int period;
-		int covertime = 0;//覆盖时间
 		int lasttime = 0;//覆盖时间
-		int coverage;//覆盖率
+		int covertime = 0;//覆盖时间
+		int coverage;//覆盖率，万分数
 	};
-	std::unordered_map<std::pair<int64, int>, elem> pools;
+	struct PoolKeyHash
+	{
+		size_t operator()(const std::pair<int64, int> &val) const
+		{
+			return std::hash<int64>()(val.first) + std::hash<int>()(val.second);
+		}
+	};
+	std::unordered_map<std::pair<int64, int>, Elem, PoolKeyHash> pool;
 
-	struct buff_set_content
+	struct BuffSetElem
 	{
 		bool active;
 		std::unordered_set<int64> roles;
 		std::vector<std::pair<int, int64>> sorted_roles;//按覆盖率排序的roles
 	};
-	std::unordered_map<int, buff_set_content> buff_set;
-	struct buff_sort_content
+	std::unordered_map<int, BuffSetElem> buff_set;
+
+	struct BuffSortElem
 	{
 		int buffid;
 		int count;
-		buff_sort_content(int a, int b) : buffid(a), count(b) {}
+		BuffSortElem(int b, int c) : buffid(b), count(c) {}
 	};
-	std::vector<buff_sort_content> buff_sort;//按数量排序的buffs，由buff_set得来
-	std::vector<buff_sort_content> debuff_sort;//按数量排序的debuffs，由buff_set得来
+	std::vector<BuffSortElem> buff_sort;//按数量排序的buffs，由buff_set得来
+	std::vector<BuffSortElem> debuff_sort;//按数量排序的debuffs，由buff_set得来
 
-	struct role_set_content
+	struct RoleSetElem
 	{
 		std::unordered_set<int> buffs;
 		std::vector<std::pair<int, int>> sorted_buffs;//按覆盖率排序的增益
 		std::vector<std::pair<int, int>> sorted_debuffs;//按覆盖率排序的减益
 	};
-	std::unordered_map<int64, role_set_content> role_set;
-	struct role_sort_content
+	std::unordered_map<int64, RoleSetElem> role_set;
+
+	struct RoleSortElem
 	{
 		int64 roleid;
-		int coverage;//平均覆盖率
-		int count;//数量
-		role_sort_content(int64 a, int b, int c) : roleid(a), coverage(b), count(c) {}
+		int avgcoverage;//平均覆盖率
+		int buffcount;//buff数量
+		RoleSortElem(int64 r, int a, int b) : roleid(r), avgcoverage(a), buffcount(b) {}
 	};
-	std::vector<role_sort_content> role_sort1;//按平均覆盖率排序的roles，由role_set得来
-	std::vector<role_sort_content> role_sort2;//按平均覆盖率排序的roles，由role_set得来
+	std::vector<RoleSortElem> role_sort1;//按buff平均覆盖率排序的roles，由role_set得来
+	std::vector<RoleSortElem> role_sort2;//按debuff平均覆盖率排序的roles，由role_set得来
 
 	bool OK = false;
 	int64 starttime = 0;
 
 public:
+	void begin(int64 nowtime) { starttime = nowtime; }
+	void finish() { calbuff(true); }
 	void addbuff(int64 roleid, int buffid, int64 addtime, int period);
 	void delbuff(int64 roleid, int buffid);
 	bool calbuff(bool finish);
 	BuffCover& operator+=(const BuffCover &cover);
-	//一个名字和职业的缓存
 };
 
 struct Battle
 {
 	BuffCover _buff;
-	void finish()
-	{
-		_buff.calbuff(true);
-	}
+	void begin(int64 nowtime) { _buff.begin(nowtime); }
+	void finish() { _buff.finish(); }
 };
 
 }
