fd995386e67af408101e523e5fe281bae4a6826f
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-09-15 14:09:58 +0000

    git-svn-id: svn://154.8.199.216/files@305 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- lheapchecker.h
+++ lheapchecker.h
@@ -25,39 +25,43 @@ void operator delete(void *p)
 
 class lheapchecker
 {
-	lmap<lstring, int> call_counter;
+	lmap<void*, int> call_counter;
 	lmap<void*, int*> addr_caller;
 	lmap<void*, lstring> cache;
+	bool isopen;
+
 public:
+	lheapchecker(bool open = true) : isopen(open){}
+	void open() { isopen = true; }
+	void close() { isopen = false; }
 	void* call(void *addr)
 	{
+		if(!isopen) return addr;
 		void* buffer[3];
 		int stack_num = backtrace(buffer, 3);
 		if(stack_num < 3)
 		{
-			puts("error");
+			puts("lheapchecker backtrace error");
 			return NULL;
 		}
 
-		lstring *caller;
-		auto iter = cache.find(buffer[2]);
-		if(iter != cache.end())
-			caller = &iter->second;
-		else
+		/*
+		if(cache.find(buffer[2]) == cache.end())
 		{
 			void* temp[1] = {buffer[2]};
 			char ** stacktrace = backtrace_symbols(temp, 1);
-			caller = &cache[buffer[2]];
-			*caller = stacktrace[0];
+			//应用于部分动态库会崩溃, 不知为何
+			if(strstr(stacktrace[0], ".so("))
+			{
+				free(stacktrace);
+				return addr;
+			}
+			cache[buffer[2]] = stacktrace[0];
 			free(stacktrace);
 		}
+		*/
 
-		//应用于动态库会崩溃, 不知为何
-		if(caller->find(".so(") != std::string::npos)
-			return addr;
-		caller->erase(caller->find_first_of(' '));
-
-		int *pnum = &call_counter[*caller];
+		int *pnum = &call_counter[buffer[2]];
 		++*pnum;
 		addr_caller[addr] = pnum;
 
@@ -70,13 +74,34 @@ public:
 			--*iter->second;
 	}
 
-	void checkto(int fd = 1)
+	void symbols()
 	{
-		for(lmap<lstring, int>::iterator it = call_counter.begin(), ie = call_counter.end(); it != ie; ++it)
+		for(lmap<void*, int>::iterator it = call_counter.begin(), ie = call_counter.end(); it != ie; ++it)
+		{
+			if(cache.find(it->first) == cache.end())
+			{
+				void* temp[1] = {it->first};
+				char ** stacktrace = backtrace_symbols(temp, 1);
+				//应用于部分动态库会崩溃, 不知为何
+				if(strstr(stacktrace[0], ".so("))
+				{
+					free(stacktrace);
+					cache[it->first] = "so";
+					break;
+				}
+				cache[it->first] = stacktrace[0];
+				free(stacktrace);
+			}
+		}
+	}
+	void dumpto(int fd = 1)
+	{
+		symbols();
+		for(lmap<void*, int>::iterator it = call_counter.begin(), ie = call_counter.end(); it != ie; ++it)
 		{
 			if(it->second != 0)
 			{
-				write(fd, it->first.data(), it->first.length());
+				write(fd, cache[it->first].data(), cache[it->first].length());
 				char buffer[16];
 				sprintf(buffer, ":%d\n", it->second);
 				write(fd, buffer, strlen(buffer));
