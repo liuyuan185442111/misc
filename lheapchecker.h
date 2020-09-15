6e293c7ef027b2a87872848a6e73a8bfe5022ac1
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-09-15 09:41:53 +0000

    git-svn-id: svn://154.8.199.216/files@304 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- lheapchecker.h
+++ lheapchecker.h
@@ -27,6 +27,7 @@ class lheapchecker
 {
 	lmap<lstring, int> call_counter;
 	lmap<void*, int*> addr_caller;
+	lmap<void*, lstring> cache;
 public:
 	void* call(void *addr)
 	{
@@ -38,15 +39,25 @@ public:
 			return NULL;
 		}
 
-		char ** stacktrace = backtrace_symbols(buffer, stack_num);
-		lstring caller(stacktrace[2]);
-		free(stacktrace);
-		//应用于动态库会崩溃, 不知为何
-		if(caller.find(".so(") != std::string::npos)
-			return addr;
-		caller.erase(caller.find_first_of(' '));
+		lstring *caller;
+		auto iter = cache.find(buffer[2]);
+		if(iter != cache.end())
+			caller = &iter->second;
+		else
+		{
+			void* temp[1] = {buffer[2]};
+			char ** stacktrace = backtrace_symbols(temp, 1);
+			caller = &cache[buffer[2]];
+			*caller = stacktrace[0];
+			free(stacktrace);
+		}
 
-		int *pnum = &call_counter[caller];
+		//应用于动态库会崩溃, 不知为何
+		if(caller->find(".so(") != std::string::npos)
+			return addr;
+		caller->erase(caller->find_first_of(' '));
+
+		int *pnum = &call_counter[*caller];
 		++*pnum;
 		addr_caller[addr] = pnum;
 
