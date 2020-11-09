6ca4dcf52196a7d873a03d99b4099c453ee4e64b
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-11-09 03:20:03 +0000

    git-svn-id: svn://154.8.199.216/files@315 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- ltrie.h
+++ ltrie.h
@@ -1,62 +1,97 @@
 #ifndef LTRIE_H_
 #define LTRIE_H_
 
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <utility>
+
 struct ltrie
 {
 
-struct Node
+struct TrieNode
 {
 	char value;
-	std::unordered_map<char, Node*> children;
-	//std::map<char, Node*> children;
-	Node *parent;
-	//Node *fail = nullptr;
-	bool leaf = false;
-	Node() : value(0), parent(nullptr){}
-	Node(char v, Node* p) : value(v), parent(p){}
+	bool isleaf = false;
+	//std::unordered_map<char, TrieNode*> children;
+	std::map<char, TrieNode*> children;
+	TrieNode *parent;//用于回溯和删除
+	TrieNode *fail = nullptr;
+	TrieNode() : value(0), parent(nullptr){}
+	TrieNode(char v, TrieNode* p) : value(v), parent(p){}
 };
 
-static Node* init()
+static const TrieNode* init_trie()
 {
-	return new Node;
+	return new TrieNode;
 }
 
-static void add(Node *root, const std::string &str)
+static void add_str(const TrieNode *root, const std::string &str)
 {
+	TrieNode* node = const_cast<TrieNode*>(root);
 	for(char c : str)
 	{
-		if(root->children.find(c) == root->children.end())
+		auto ret = node->children.emplace(c, nullptr);
+		if(ret.second)
 		{
-			root->children[c] = new Node(c, root);
+			node = new TrieNode(c, node);
+			ret.first->second = node;
 		}
-		root = root->children[c];
+		else
+		{
+			node = ret.first->second;
+		}
 	}
-	root->leaf = true;
+	node->isleaf = true;
 }
 
-static void del(Node *root, const std::string &str)
+static bool search(const TrieNode *root, const std::string &str)
 {
-	if(Node *target = search(root, str))
+	for(auto c : str)
+	{
+		auto iter = root->children.find(c);
+		if(iter == root->children.end())
+			return false;
+		root = iter->second;
+	}
+	return root->isleaf;
+}
+
+static const TrieNode* find(const TrieNode *root, const std::string &str)
+{
+	for(auto c : str)
+	{
+		auto iter = root->children.find(c);
+		if(iter == root->children.end())
+			return nullptr;
+		root = iter->second;
+	}
+	return root->isleaf ? root : nullptr;
+}
+
+static void del_str(const TrieNode *root, const std::string &str)
+{
+	if(TrieNode *target = const_cast<TrieNode*>(find(root, str)))
 	{
 		if(!target->children.empty())
 		{
-			target->leaf = false;
+			target->isleaf = false;
 			return;
 		}
 		while(target->parent)
 		{
-			if(target->parent->children.size() == 1)
+			auto tmp = target->parent;
+			if(tmp->children.size() == 1)
 			{
-				Node *tmp = target->parent;
-				free(target);
 				//非root不用clear，因为随后他自己也会被free掉
-				if(tmp == root)
-					tmp->children.clear();
+				if(tmp == root) tmp->children.clear();
+				free(target);
 				target = tmp;
 			}
 			else
 			{
-				target->parent->children.erase(target->value);
+				tmp->children.erase(target->value);
 				free(target);
 				return;
 			}
@@ -64,31 +99,30 @@ static void del(Node *root, const std::string &str)
 	}
 }
 
-static std::string backtracking(Node *now)
+static std::string backtrack(const TrieNode *node)
 {
 	std::string str;
 	do
 	{
-		str.push_back(now->value);
-		now = now->parent;
-	}
-	while(now);
+		str.push_back(node->value);
+		node = node->parent;
+	} while(node);
 	std::reverse(str.begin(), str.end());
-	return str;
+	return std::move(str);
 }
 
-static void bfs(Node *root)
+static void bfs_trie(const TrieNode *root)
 {
-	std::queue<Node*> q;
+	std::queue<decltype(root)> q;
 	q.push(root);
 	while(!q.empty())
 	{
 		root = q.front();
-		if(root->leaf)
+		if(root->isleaf)
 		{
-			cout << "leaf:" << backtracking(root) << endl;
+			cout << "leaf " << backtrack(root) << endl;
 		}
-		for(const auto &child : root->children)
+		for(auto &child : root->children)
 		{
 			cout << "bfs " << child.first << endl;
 			q.push(child.second);
@@ -97,46 +131,20 @@ static void bfs(Node *root)
 	}
 }
 
-static void release(Node *root)
+static void free_trie(const TrieNode *root)
 {
-	std::queue<Node*> q;
+	std::queue<decltype(root)> q;
 	q.push(root);
 	while(!q.empty())
 	{
 		root = q.front();
-		for(const auto &child : root->children)
+		for(auto &child : root->children)
 		{
 			q.push(child.second);
 		}
-		free(root);
+		free((void*)root);
 		q.pop();
 	}
-}
-
-static bool has(Node *root, const std::string &str)
-{
-	for(char c : str)
-	{
-		if(root->children.find(c) == root->children.end())
-		{
-			return false;
-		}
-		root = root->children[c];
-	}
-	return root->leaf;
-}
-
-static Node* search(Node *root, const std::string &str)
-{
-	for(char c : str)
-	{
-		if(root->children.find(c) == root->children.end())
-		{
-			return nullptr;
-		}
-		root = root->children[c];
-	}
-	return root->leaf ? root : nullptr;
 }
 
 };
