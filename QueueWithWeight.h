ee264e16fcb74afa991be11a040344aa8436a6cf
eye <eye@91d82e07-cc47-4fd4-84ae-454f48a309b4>
2020-11-16 03:49:04 +0000

    git-svn-id: svn://154.8.199.216/files@320 91d82e07-cc47-4fd4-84ae-454f48a309b4

--- QueueWithWeight.h
+++ QueueWithWeight.h
@@ -2,80 +2,99 @@
 #define QUEUEWITHWEIGHT_H
 
 #include <vector>
-#include <queue>
+#include <list>
+#include <map>
+#include <set>
+
+using TIMETYPE = time_t;
 
 //可用于游戏排队
 template <typename T>
 class QueueWithWeight
 {
-    std::vector<unsigned> _weights;
-    std::vector<std::queue<T>> _queues;
-    std::queue<T> *_major = nullptr;
+	using ListNode = typename std::list<T>::iterator;
+	struct State
+	{
+		unsigned index;
+		ListNode node;
+		TIMETYPE offlinetime = 0;
+		State(unsigned i, ListNode n) : index(i), node(n) {}
+	};
+	std::map<T, State> _all;
+	std::vector<unsigned> _weights;
+	std::vector<std::list<T>> _queues;
 public:
-    QueueWithWeight(const std::vector<unsigned> &weights={})
-    {
-        if(weights.empty())
-        {
-            _queues.emplace_back(std::queue<T>());
-            _major = &_queues[0];
-            return;
-        }
-        _weights = weights;
-        _queues.assign(weights.size(), std::queue<T>());
-    }
-    bool push(T t, unsigned index=0)
-    {
-        if(_major)
-        {
-            _major->push(t);
-            return true;
-        }
-        if(index < _queues.size())
-        {
-            _queues[index].push(t);
-            return true;
-        }
-        return false;
-    }
-    bool empty()
-    {
-        for(const auto &q : _queues)
-        {
-            if(!q.empty())
-                return false;
-        }
-        return true;
-    }
-    T pop()
-    {
-        if(_major)
-        {
-            T t = _major->front();
-            _major->pop();
-            return t;
-        }
-        std::vector<unsigned> weights(_weights);
-        unsigned weight_sum=0;
-        for(int i=_queues.size(); i; --i)
-        {
-            if(_queues[i-1].empty())
-                weights[i-1] = 0;
-            else
-                weight_sum += weights[i-1];
-        }
-        int pos = rand() % weight_sum;
-        for(int i=weights.size(); i; --i)
-        {
-            if(weights[i-1] > pos)
-            {
-                T t = _queues[i-1].front();
-                _queues[i-1].pop();
-                return t;
-            }
-            pos -= weights[i-1];
-        }
-        return 0;
-    }
+	QueueWithWeight(const std::vector<unsigned> &weights={})
+	{
+		if(weights.empty())
+		{
+			_queues.emplace_back(std::queue<T>());
+			return;
+		}
+		_weights = weights;
+		_queues.assign(weights.size(), std::queue<T>());
+	}
+	bool push(T t, unsigned index=0)
+	{
+		if(index >= _queues.size())
+			return false;
+		leave(t);
+		_queues[index].push_back(t);
+		_all.emplace(t, index, --_queues[index].end());
+		return true;
+	}
+	bool empty()
+	{
+		for(const auto &q : _queues)
+		{
+			if(!q.empty())
+				return false;
+		}
+		return true;
+	}
+	T pop()
+	{
+		if(_queues.size() == 1)
+		{
+			T t = _queues[0]->front();
+			_queues[0]->pop_front();
+			_all.erase(t);
+			return t;
+		}
+		std::vector<unsigned> weights(_weights);
+		unsigned weight_sum=0;
+		for(int i=_queues.size(); i; --i)
+		{
+			if(_queues[i-1].empty())
+				weights[i-1] = 0;
+			else
+				weight_sum += weights[i-1];
+		}
+		int pos = rand() % weight_sum;
+		for(int i=weights.size(); i; --i)
+		{
+			if(weights[i-1] > pos)
+			{
+				T t = _queues[i-1].front();
+				_queues[i-1].pop_front();
+				_all.erase(t);
+				return t;
+			}
+			pos -= weights[i-1];
+		}
+		return 0;
+	}
+	void offline()
+	{
+	}
+	void leave(T t)
+	{
+		auto iter = _all.find(t);
+		if(iter == _all.end())
+			return;
+		_queues[iter->index].erase(iter->node);
+		_all.erase(iter);
+	}
 };
 
 #endif // QUEUEWITHWEIGHT_H
